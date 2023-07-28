d7341c53f61be884b2324fe67fbe68df719054ec
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2023-07-28 10:10:50 -0400

    Adding examples of int, char, double, and string

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,12 @@
 int main(){
   std::cout << "I like pizza!" << '\n';
   std::cout << "It's really good!" << std::endl;
+
+  int age = 45;
+  char currency = '$';
+  double total = 15890.78;
+  std::string name = "Elmehdi";
+
+  std::cout << "Hello " << name << " your age is " << age << " and you have " << currency << total;
   return 0;
 }
