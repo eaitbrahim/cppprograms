59aaa6c4e518a1f8dbb473696d246f5202fd8eff
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2023-07-28 16:52:46 -0400

    Create an example of type def to create addional name for another data type.

---
+++ typedef.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+using text_t = std::string;
+using number_t = int;
+
+int main(){
+  text_t firstName= "Bro";
+  number_t age = 21;
+
+  std::cout << firstName << '\n';
+  std::cout << age << '\n';
+
+  return 0;
+}
