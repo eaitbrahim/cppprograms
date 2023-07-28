4a9e46077157f89b5a41946c8d2f5d0499945885
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2023-07-28 17:18:29 -0400

    Deal with white space buffer when using get line

--- askingQuestions.cpp
+++ askingQuestions.cpp
@@ -3,12 +3,18 @@
 int main(){
   std::string name;
   int age;
+  std::string address;
 
-  std::cout << "What's yourfull name?" << "\n";
-  std::getline(std::cin, name);
+  std::cout << "What's your name?" << "\n";
+  std::cin >> name;
+
   std::cout << "What's your age?" << "\n";
   std::cin >> age;
 
-  std::cout << "Hello " << name << ", your age is " << age;
+  std::cout << "What's is your address?" << '\n';
+  std::getline(std::cin >> std::ws, address);
+
+  std::cout << "Hello " << name << ", your age is " << age << "\n";
+  std::cout << "Your address is " << address;
   return 0;
 }
