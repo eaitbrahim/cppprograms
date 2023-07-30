418cb0ce3a900b8838e5880106032c07b746e7e6
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2023-07-30 18:46:14 -0400

    some examples of math functions

---
+++ mathFunction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+  std::cout << std::max(3, 5) << "\n";
+  std::cout << std::min(3, 4) << "\n";
+  std::cout << std::pow(2, 4) << "\n";
+  std::cout << std::sqrt(9) << "\n";
+  std::cout << std:: abs(-3) << "\n";
+  std::cout << std::round(3.14) << "\n";
+  std::cout << std::ceil(3.99) << "\n";
+  return 0;
+}
